a34119d505bb898ffe365946703c0a57c29fed8f
Teemu Leskinen <t8lete03@students.oamk.fi>
2020-11-17 20:45:25 +0200

    Login button functionality

--- app/src/main/java/fi/example/proto/mobiiliproject/ui/Sign_in/Sign_inFragment.java
+++ app/src/main/java/fi/example/proto/mobiiliproject/ui/Sign_in/Sign_inFragment.java
@@ -1,27 +1,54 @@
 package fi.example.proto.mobiiliproject.ui.Sign_in;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
+import androidx.navigation.Navigation;
+
+import com.google.android.material.snackbar.Snackbar;
 
 import fi.example.proto.mobiiliproject.R;
+import fi.example.proto.mobiiliproject.ui.home.HomeFragment;
+import fi.example.proto.mobiiliproject.ui.products.ProductsFragment;
 
 public class Sign_inFragment extends Fragment {
+
+    Button btnLogIn;
+
+    public Sign_inFragment(){
+        //Required empty public constructor
+    }
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
         Sign_inViewModel sign_inViewModel = new ViewModelProvider(this).get(Sign_inViewModel.class);
-        View root = inflater.inflate(R.layout.fragment_sign_in, container, false);
-        //final TextView textView = root.findViewById(R.id.text_sign_in);
-        //sign_inViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
-        return root;
+        View view_log_in = inflater.inflate(R.layout.fragment_sign_in, container, false);
+
+    btnLogIn = view_log_in.findViewById(R.id.btn_login) ;
+
+    btnLogIn.setOnClickListener(new View.OnClickListener() {
+        @Override
+        public void onClick(View view) {
+            Navigation.findNavController(view_log_in).navigate(R.id.action_nav_sign_in_to_nav_home);
+
+        }
+    });
+
+
+        return view_log_in;
+
     }
 }
