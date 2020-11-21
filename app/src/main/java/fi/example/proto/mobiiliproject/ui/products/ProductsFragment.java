3ec265282749ef90d13da5bf2c417d5fcd89ab12
Marianne <t8kama06@students.oamk.fi>
2020-11-22 01:48:45 +0200

    Navigation link working now

--- app/src/main/java/fi/example/proto/mobiiliproject/ui/products/ProductsFragment.java
+++ app/src/main/java/fi/example/proto/mobiiliproject/ui/products/ProductsFragment.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.TextView;
 
 
@@ -14,15 +15,24 @@ import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
 
 import fi.example.proto.mobiiliproject.R;
+import fi.example.proto.mobiiliproject.ui.home.HomeFragment;
+import fi.example.proto.mobiiliproject.ui.products.ProductsFragment;
 
 public class ProductsFragment extends Fragment {
+
+    public ProductsFragment(){
+        //empty public contructor
+    }
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
         ProductsViewModel productsViewModel = new ViewModelProvider(this).get(ProductsViewModel.class);
-        View root = inflater.inflate(R.layout.fragment_products, container, false);
-        final TextView textView = root.findViewById(R.id.text_products);
-        productsViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
-        return root;
+        View viewProducts = inflater.inflate(R.layout.fragment_products, container, false);
+
+
+
+       /* final TextView textView = root.findViewById(R.id.text_products);
+        productsViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);*/
+        return viewProducts;
     }
 }
