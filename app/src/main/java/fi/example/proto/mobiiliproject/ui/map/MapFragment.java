da6f3e8459d3316c060b2718b2d66da798fdf493
Marianne <t8kama06@students.oamk.fi>
2020-12-06 01:02:57 +0200

    Buttons added on fragment_products xml

--- app/src/main/java/fi/example/proto/mobiiliproject/ui/map/MapFragment.java
+++ app/src/main/java/fi/example/proto/mobiiliproject/ui/map/MapFragment.java
@@ -1,33 +1,37 @@
 package fi.example.proto.mobiiliproject.ui.map;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-
+import android.widget.Button;
 import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
-import com.google.android.gms.maps.CameraUpdateFactory;
-import com.google.android.gms.maps.GoogleMap;
-import com.google.android.gms.maps.MapView;
-import com.google.android.gms.maps.OnMapReadyCallback;
-import com.google.android.gms.maps.model.LatLng;
 
 import fi.example.proto.mobiiliproject.R;
 
 public class MapFragment extends Fragment {
-
-    MapView mapV;
-    GoogleMap map;
-
+    Button buttonMap;
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
 
         View mapView = inflater.inflate(R.layout.fragment_map, container, false);
+        buttonMap = (Button) mapView.findViewById(R.id.button_map);
+        buttonMap.setOnClickListener(new View.OnClickListener() {
 
+            @Override
+            public void onClick(View v) {
+                openMap();
+            }
+        });
 
         return mapView;
     }
 
+    private void openMap() {
+        Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("geo:65.013384,25.4651043?z=14"));
+        startActivity(intent);
+    }
 }
