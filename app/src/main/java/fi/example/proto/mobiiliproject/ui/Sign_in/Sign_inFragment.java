c839944f69ecaa32653c54a83e7dbc7f0677851b
Teemu Leskinen <t8lete03@students.oamk.fi>
2020-12-06 14:00:04 +0200

    Login connection to database

--- app/src/main/java/fi/example/proto/mobiiliproject/ui/Sign_in/Sign_inFragment.java
+++ app/src/main/java/fi/example/proto/mobiiliproject/ui/Sign_in/Sign_inFragment.java
@@ -17,6 +17,7 @@ import androidx.lifecycle.ViewModelProvider;
 import androidx.navigation.Navigation;
 
 
+import com.vishnusivadas.advanced_httpurlconnection.PutData;
 
 import fi.example.proto.mobiiliproject.R;
 
@@ -25,7 +26,7 @@ public class Sign_inFragment extends Fragment {
     Button btnLogIn; 
     TextView txtViewRegister;
 
-    EditText text_input_first_name, text_input_last_name, text_input_email, text_input_username, text_input_password ;
+    EditText text_input_username, text_input_password ;
 
     public Sign_inFragment(){
         //Required empty public constructor
@@ -36,16 +37,58 @@ public class Sign_inFragment extends Fragment {
         Sign_inViewModel sign_inViewModel = new ViewModelProvider(this).get(Sign_inViewModel.class);
         View view_log_in = inflater.inflate(R.layout.fragment_sign_in, container, false);
 
-    btnLogIn = view_log_in.findViewById(R.id.btn_login) ;
-    txtViewRegister = view_log_in.findViewById(R.id.textview_register) ;
+        text_input_username = view_log_in.findViewById(R.id.edit_text_username);
+        text_input_password = view_log_in.findViewById(R.id.edit_text_password);
+        btnLogIn = view_log_in.findViewById(R.id.btn_login) ;
+        txtViewRegister = view_log_in.findViewById(R.id.textview_register) ;
 
-    btnLogIn.setOnClickListener(new View.OnClickListener() {
+        btnLogIn.setOnClickListener(new View.OnClickListener() {
         @Override
-        public void onClick(View view) {
+            public void onClick(View view) {
 
-            Navigation.findNavController(view_log_in).navigate(R.id.action_nav_sign_in_to_nav_home);
+            final String username, password;
+
+            username = String.valueOf(text_input_username.getText());
+            password = String.valueOf(text_input_password.getText());
+
+            if(!username.equals("") && !password.equals("")) {
+
+                Handler handler = new Handler(Looper.getMainLooper());
+                handler.post(new Runnable() {
+                    @Override
+                    public void run() {
+
+                        String[] field = new String[2];
+                        field[0]= "username";
+                        field[1]= "password";
+
+                        String[] data = new String[2];
+                        data[0] = username;
+                        data[1] = password;
+
+                        PutData putData = new PutData("http://192.168.1.6:1025/mobileProject/login.php", "POST", field, data);
+                        if (putData.startPut()) {
+                            if (putData.onComplete()) {
+                                String result = putData.getResult();
+                                if (result.equals("Kirjautuminen onnistui")) {
+                                    Toast.makeText(getContext(), result, Toast.LENGTH_SHORT).show();
+                                    Navigation.findNavController(view_log_in).navigate(R.id.action_nav_sign_in_to_nav_home);
+                                } else {
+                                    Toast.makeText(getContext(), result, Toast.LENGTH_SHORT).show();
+                                }
+                            }
+                        }
+
+                    }
+                });
+            }
+            else {
+                Toast.makeText(getContext(), "Täytä kaikki kentät", Toast.LENGTH_SHORT).show();
+            }
         }
-    });
+
+        });
+
 
     txtViewRegister.setOnClickListener(new View.OnClickListener() {
         @Override
