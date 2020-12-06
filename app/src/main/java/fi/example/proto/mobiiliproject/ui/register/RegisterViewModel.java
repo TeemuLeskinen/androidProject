05ab7f809fb98b5fa88b690dceffe474267d907e
Teemu Leskinen <t8lete03@students.oamk.fi>
2020-12-06 11:20:25 +0200

    Login button functionality

---
+++ app/src/main/java/fi/example/proto/mobiiliproject/ui/register/RegisterViewModel.java
@@ -0,0 +1,7 @@
+package fi.example.proto.mobiiliproject.ui.register;
+
+import androidx.lifecycle.ViewModel;
+
+public class RegisterViewModel extends ViewModel {
+    // TODO: Implement the ViewModel
+}
