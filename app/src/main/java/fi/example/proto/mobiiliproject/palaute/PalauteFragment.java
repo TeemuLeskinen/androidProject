f6e6a8765e7d695c5b4f61052556f1b4be3a86be
formam <t8foju00@students.oamk.fi>
2020-12-10 10:57:50 +0200

    Yritystilaus

--- app/src/main/java/fi/example/proto/mobiiliproject/palaute/PalauteFragment.java
+++ app/src/main/java/fi/example/proto/mobiiliproject/palaute/PalauteFragment.java
@@ -11,13 +11,9 @@ import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
-import androidx.lifecycle.Observer;
-import androidx.lifecycle.ViewModelProvider;
 
 import fi.example.proto.mobiiliproject.R;
 //import fi.example.proto.mobiiliproject.palaute.PalauteViewModel;
