2e3d4a8eee0d11e7f6f6bbafe05f169d921a73e7
t8kama06 <t8kama06@students.oamk.fi>
2020-12-11 22:04:16 +0200

    kommentit lisätty

--- app/src/main/java/fi/example/proto/mobiiliproject/ui/products/ProductsFragment.java
+++ app/src/main/java/fi/example/proto/mobiiliproject/ui/products/ProductsFragment.java
@@ -13,6 +13,7 @@ import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
+import androidx.navigation.Navigation;
 
 import fi.example.proto.mobiiliproject.R;
 import fi.example.proto.mobiiliproject.ui.home.HomeFragment;
@@ -20,35 +21,67 @@ import fi.example.proto.mobiiliproject.ui.products.ProductsFragment;
 
 public class ProductsFragment extends Fragment {
 
-    Button buttonOrder1, buttonOrder2;
-
-    public ProductsFragment(){
-        //empty public contructor
-    }
-
+    Button buttonOrder1, buttonOrder2, buttonOrder3, buttonOrder4, buttonOrder5, buttonOrder6;
+    //luodaan view
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
         ProductsViewModel productsViewModel = new ViewModelProvider(this).get(ProductsViewModel.class);
         View viewProducts = inflater.inflate(R.layout.fragment_products, container, false);
 
+        //haetaan buttonit tuotteille
         buttonOrder1 = (Button) viewProducts.findViewById(R.id.buttonOrder1);
+        buttonOrder2 = (Button) viewProducts.findViewById(R.id.buttonOrder2);
+        buttonOrder3 = (Button) viewProducts.findViewById(R.id.buttonOrder3);
+        buttonOrder4 = (Button) viewProducts.findViewById(R.id.buttonOrder4);
+        buttonOrder5 = (Button) viewProducts.findViewById(R.id.buttonOrder5);
+        buttonOrder6 = (Button) viewProducts.findViewById(R.id.buttonOrder6);
+
+
+        //asetetaan onClickListener, jonka avulla nappia klikkaamalla siirrytään tuotteista action_nav_products... actionin avulla order sivulle
         buttonOrder1.setOnClickListener(new View.OnClickListener(){
             public void onClick(View v){
-                //
-            }
-        });
 
-        buttonOrder2 = (Button) viewProducts.findViewById(R.id.buttonOrder2);
+                    Navigation.findNavController(viewProducts).navigate(R.id.action_nav_products_to_nav_order);
+                }
+            });
+
+
         buttonOrder2.setOnClickListener(new View.OnClickListener(){
             public void onClick(View v){
-                //
+
+                Navigation.findNavController(viewProducts).navigate(R.id.action_nav_products_to_nav_order);
             }
         });
 
+        buttonOrder3.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View v){
 
+                Navigation.findNavController(viewProducts).navigate(R.id.action_nav_products_to_nav_order);
+            }
+        });
 
-       /* final TextView textView = root.findViewById(R.id.text_products);
-        productsViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);*/
+        buttonOrder4.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View v){
+
+                Navigation.findNavController(viewProducts).navigate(R.id.action_nav_products_to_nav_order);
+            }
+        });
+
+        buttonOrder5.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View v){
+
+                Navigation.findNavController(viewProducts).navigate(R.id.action_nav_products_to_nav_order);
+            }
+        });
+
+        buttonOrder6.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View v){
+
+                Navigation.findNavController(viewProducts).navigate(R.id.action_nav_products_to_nav_order);
+            }
+        });
+
         return viewProducts;
     }
+
 }
